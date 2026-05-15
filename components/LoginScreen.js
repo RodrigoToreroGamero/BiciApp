ab20fd696dc0f6f4992470ccae5117ad45f8ae50
RodrigoToreroGamero <132959597+RodrigoToreroGamero@users.noreply.github.com>
2026-05-15 17:03:15 -0500

    Validation added for usercode and password

--- components/LoginScreen.js
+++ components/LoginScreen.js
@@ -4,39 +4,59 @@ import { globalStyles } from './globalStyles';
 import { regexPatterns } from './regexPatterns';
 
 export default function LoginScreen({ setCurrentScreen }) {
+		
+	const [usercode, setUsercode] = useState('');
+	const [password, setPassword] = useState('');
+	const [usercodeError, setUsercodeError] = useState('');
+	const [passwordError, setPasswordError] = useState('');
+	
+	
+	const validateUsercode = (text) => {
+		setUsercode(text);
+		if(!regexPatterns.usercode.regex.test(text)) {
+			setUsercodeError(regexPatterns.usercode.errorMsg);
+		} else {
+			setUsercodeError('');
+		}	
+	};
+	
+	const validatePassword = (text) => {
+		setPassword(text);
+		if(text.trim() === "") {
+			setPasswordError(regexPatterns.password.errorMsg);
+		} else {
+			setPasswordError('');
+		}	
+	};
+	
 	
 	return (
-		<View
-		style={globalStyles.container}
-		>
+		<View style={globalStyles.container}>
 			<View>
-				<Text
-				style={globalStyles.titleText}>
-					Iniciar Sesión
-				</Text>
+				<Text style={globalStyles.titleText}>Iniciar Sesión</Text>
 			</View>
-			<View
-			style={globalStyles.background}>
-				<TextInput
-				style={globalStyles.input}
-				placeholder='Código de usuario'
+			<View style={globalStyles.background}>
+				<TextInput style={globalStyles.input}
+					placeholder='Código de usuario'
+					value={usercode}
+					onChangeText={validateUsercode}
 				/>
-				<TextInput
-				style={globalStyles.input}
-				placeholder='Contraseña'
+				{usercodeError ? <Text style={{color: 'red'}}>{regexPatterns.usercode.errorMsg}</Text> : null}
+				
+				<TextInput style={globalStyles.input}
+					placeholder='Contraseña'
+					value={password}
+					onChangeText={validatePassword}
 				/>
-				<TouchableOpacity
-				style={globalStyles.loginBtn}
-				onPress={()=> setCurrentScreen('select')}				
+				{passwordError ? <Text style={{color: 'red'}}>{regexPatterns.password.errorMsg}</Text> : null}
+				
+				<TouchableOpacity style={globalStyles.loginBtn}
+					onPress={()=> setCurrentScreen('select')}				
 				>
-					<Text
-					style={globalStyles.loginTxt}>
-						Ingresar
-					</Text>
+					<Text style={globalStyles.loginTxt}>Ingresar</Text>
 				</TouchableOpacity>
-				<Text
-				style={globalStyles.forgotPassword}
-				onPress={()=> Linking.openURL('')}
+				<Text style={globalStyles.forgotPassword}
+					onPress={()=> Linking.openURL('')}
 				>
 					Restablecer Contraseña
 				</Text>
--- components/regexPatterns.js
+++ components/regexPatterns.js
@@ -1,4 +1,3 @@
-
 export const regexPatterns = {
 	alphabet: {
 		id: "alphabet",
@@ -10,6 +9,11 @@ export const regexPatterns = {
 		// https://saturncloud.io/blog/how-can-i-validate-an-email-address-using-a-regular-expression/
 		regex: /^([a-zA-Z0-9._%-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})$/, 
 		errorMsg: "Correo inválido"
+	},
+	usercode: {
+		id: "usercode",
+		regex: /^[UCS][0-9]{8}$/,
+		errorMsg: "Código de usuario inválido"
 	},
 	password: {
 		id: "password",
