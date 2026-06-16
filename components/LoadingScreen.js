942f380e8367728c58d81c0032e190f205a3e424
RodrigoToreroGamero <132959597+RodrigoToreroGamero@users.noreply.github.com>
2026-06-16 12:00:00 -0500

    Loading screen added to Login screen and Select screen. Eye button added to password in Login screen.

---
+++ components/LoadingScreen.js
@@ -0,0 +1,22 @@
+import React, { useRef, useEffect } from 'react';
+import { Animated, View, Text, ActivityIndicator } from 'react-native';
+
+export default function LoadingScreen({ visible }) {
+	const fadeAnimation = useRef(new Animated.Value(0)).current;
+	
+	useEffect(() => {
+		Animated.timing(
+			fadeAnimation, {
+				toValue: visible ? 1 : 0,
+				duration: 300,
+				useNativeDriver: true,
+		}).start();		
+	}, [visible]);
+	
+	return (
+		<Animated.View>
+			<ActivityIndicator size="large" color="#fff" />
+			<Text>Cargando...</Text>
+		</Animated.View>
+	);
+}
